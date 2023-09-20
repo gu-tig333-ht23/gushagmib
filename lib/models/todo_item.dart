36a72062b1f63a842d61db2f1f3bfd509145babf
rapunschel <rapunschel.gaming@gmail.com>
2023-09-20 15:30:53 +0200

    Edited todo_item to be compatible with the API

--- lib/models/todo_item.dart
+++ lib/models/todo_item.dart
@@ -1,16 +1,13 @@
 class ToDoItem {
   final String _text;
-  bool _isDone = false;
+  String _id;
+  bool _isDone;
 
-  ToDoItem(this._text);
+  //ToDoItem(this._text);
+  ToDoItem(this._text, [this._id = "Test", this._isDone = false]);
 
-  String getText() {
-    return _text;
-  }
-
-  void setIsDone(bool isDone) {
-    _isDone = isDone;
-  }
-
+  set done(bool value) => _isDone = value;
   bool get isDone => _isDone;
+  String get id => _id;
+  String get text => _text;
 }
