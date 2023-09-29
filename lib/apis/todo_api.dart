b0fad6c74ab53324b2e7e6f2f5b9c1582130ac5e
rapunschel <rapunschel.gaming@gmail.com>
2023-09-29 07:40:59 +0200

    Moved api key and endpoint into the class and made private

--- lib/apis/todo_api.dart
+++ lib/apis/todo_api.dart
@@ -3,14 +3,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/todo_item.dart';
 
-const String API_KEY = 'abb6cf01-0a78-4af3-a786-9ea8819073fb';
-const String ENDPOINT = 'https://todoapp-api.apps.k8s.gu.se';
-
 class TodoAPI {
+  static const String _API_KEY = 'abb6cf01-0a78-4af3-a786-9ea8819073fb';
+  static const String _ENDPOINT = 'https://todoapp-api.apps.k8s.gu.se';
   static Future<void> add(TodoItem item) async {
     await http.post(
       headers: {"Content-Type": "application/json"},
-      Uri.parse('$ENDPOINT/todos?key=$API_KEY'),
+      Uri.parse('$_ENDPOINT/todos?key=$_API_KEY'),
       body: jsonEncode(item.toJson()),
     );
   }
@@ -19,7 +18,7 @@ class TodoAPI {
     String id = item.id;
     await http.delete(
       headers: {"Content-Type": "application/json"},
-      Uri.parse('$ENDPOINT/todos/$id?key=$API_KEY'),
+      Uri.parse('$_ENDPOINT/todos/$id?key=$_API_KEY'),
       body: jsonEncode(item.toJson()),
     );
   }
@@ -28,14 +27,14 @@ class TodoAPI {
     String id = item.id;
     await http.put(
       headers: {"Content-Type": "application/json"},
-      Uri.parse('$ENDPOINT/todos/$id?key=$API_KEY'),
+      Uri.parse('$_ENDPOINT/todos/$id?key=$_API_KEY'),
       body: jsonEncode(item.toJson()),
     );
   }
 
   static Future<List<TodoItem>> fetchTodoItems() async {
     http.Response response =
-        await http.get(Uri.parse('$ENDPOINT/todos?key=$API_KEY'));
+        await http.get(Uri.parse('$_ENDPOINT/todos?key=$_API_KEY'));
 
     List<dynamic> jsonContents = await jsonDecode(response.body);
     return jsonContents
